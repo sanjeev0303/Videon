b3396f508c62c9db2945cff158c44bc46f045968
sanjeev0303 <sanjeev.081021@gmail.com>
2026-09-06 19:56:31 +0530

    remove landing and main-dashboard and combine both make client

--- main-dashboard/app/api/videon/route.ts
+++
@@ -1,9 +0,0 @@
-import { handleProcessRequest } from "@videon/sdk"
-
-
-export async function POST(request:Request) {
-    return handleProcessRequest({
-        request,
-        apiKey: process.env.VIDEON_API_KEY!,
-    })
-}
